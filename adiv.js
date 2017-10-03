43c05a2a810a96299bde7ef0f6fa9dcbc375e5a8
Sandro Comes Zacares <sacoza@floridauniversitaria.es>
2017-10-03 12:34:40 +0200

    first commit

---
+++ adiv.js
@@ -0,0 +1,13 @@
+
+var numero1=prompt ('Escribe un numero:');
+var numero2=40;
+
+if (numero1<numero2) {
+  alert("TE QUEDASTE CORTO")
+}
+else if (numero1>numero2) {
+  alert("TE PASASTE")
+}
+else if (numero1=numero2) {
+  alert("ACERTASTE")
+}
